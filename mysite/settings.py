4e08f757308fdf2c2961d5e3635bff8c701597c8
Brian DeLeonardis <bdeleonardis1@gmail.com>
2018-01-10 14:27:17 -0500

    Heroku autodeploy test

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,6 +1,7 @@
 # -----------------------------------------------
 ''' THESE SETTINGS ARE NOT PRODUCTION READY '''
 # -----------------------------------------------
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
