8d993ebbefc0abfc3fb493cc55834a76f9df751e
Aditya Srivastava <aditya.srivastava@berkeley.edu>
2018-03-10 18:57:02 -0800

    primordial view and edit

--- codedoor/views.py
+++ codedoor/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Company
 
 def hello(request):
 	return render(request, 'codedoor/hello.html', {'name': 'Brian'})
 
+def create_company(request):
+	if request.method == "POST":
+		name = request.POST["name"]
+		industry = request.POST["industry"]
+		website = request.POST["website"]
+		logo = request.POST["logo"]
+		structure = request.POST["structure"]
+
+		company = Company(name=name, industry=industry, website=website, logo=logo, structure=structure)
+		company.save()
+
+		return HttpResponse("Created" + name)
+	else:
+		return render(request, "codedoor/createcompany.html")
+
+def view_company(request, pk):
+	company = Company.objects.get(pk=pk)
+	return render(request, "codedoor/viewcompany.html", {"company": company})
+
+def edit_company(request, pk):
+	return render(request, "codedoor/viewcompany.html", {"company": company})
