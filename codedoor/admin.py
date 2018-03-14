ad6f75e6f6ce941b523020793e7e9aeb3f09d9a3
William Zhou <guanlinzhou@berkeley.edu>
2018-03-13 17:32:57 -0700

    Revert "Add models and sample data"
    This reverts commit 1345482cce262c768dac9616934870675734ca82.

--- codedoor/admin.py
+++ codedoor/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
-from .models import Company, Profile, Application, Review, Question
+from .models import Dog
 
-admin.site.register(Company)
-admin.site.register(Profile)
-admin.site.register(Application)
-admin.site.register(Review)
-admin.site.register(Question)
+admin.site.register(Dog)
