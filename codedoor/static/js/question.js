937852a35317ff9a3354d63e34f61b1990479b72
Selina Feng <selina.feng99@gmail.com>
2018-03-17 01:19:12 -0700

    Question (#5)
    * created a form for question submission
    
    * added styling to questions form
    
    * editing view v1
    
    * done with prelim
    
    * done! for now
    
    * added some styling and js
    
    * settings file
    
    * fixed js
    
    * Styled edit question
    
    * fix issues outlined in pull request #5

---
+++ codedoor/static/js/question.js
@@ -0,0 +1,17 @@
+function showAAnswer() {
+    var x = document.getElementById("applicantAnswer");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function showCAnswer() {
+    var x = document.getElementById("companyAnswer");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
--- codedoor/views.py
+++ codedoor/views.py
@@ -1,7 +1,50 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from .models import Profile, Company
+from .models import Profile, Company, Question, Application
 
+def create_question(request):
+    if request.method == 'POST':
+        try:
+            question = request.POST['question']
+            comp_answer = request.POST['company answer']
+            app_answer = request.POST['applicant answer']
+        except Exception as e:
+            return HttpResponse("You did not fill out the form correctly")
+
+        q = Question(application=Application.objects.get(pk=1), question=question, applicant_answer=app_answer, actual_answer=comp_answer)
+        q.save()
+        return redirect("codedoor:view_question", pk=q.id)
+    else:
+        return render(request, 'codedoor/createQuestion.html')
+
+def edit_question(request, pk):
+    q = get_object_or_404(Question, pk=pk)
+    if request.method == 'POST':
+        try:
+            q.question = request.POST['question']
+            q.actual_answer = request.POST['company answer']
+            q.applicant_answer = request.POST['applicant answer']
+        except Exception as e:
+            return HttpResponse("You did not fill out the form correctly")
+        q.save()
+        return redirect("codedoor:view_question", pk=q.id)
+    else:
+        return render(request, 'codedoor/editQuestion.html',
+                      {
+                          "question": q.question,
+                          "applicant_answer": q.applicant_answer,
+                          "comp_answer": q.actual_answer,
+                          "pk": pk,
+                          "link": "/codedoor/editingquestion/" + str(pk)
+                      })
+
+def view_question(request, pk):
+    q = get_object_or_404(Question, pk=pk)
+    return render(request, "codedoor/viewQuestion.html", {"q": q})
+
+def list_questions(request):
+    questions = Question.objects.order_by("-pk")
+    return render(request, 'codedoor/viewQuestions.html', {"questions": questions})
 
 def createprofile(request):
     if request.method == "GET":
