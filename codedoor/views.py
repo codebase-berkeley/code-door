7d213a2ddd32477bcf450a1d345752ab4651a621
Andrew Chan <andrewkchan.akc@gmail.com>
2018-03-16 22:54:36 -0700

    fix issues outlined in pull request #4

--- codedoor/views.py
+++ codedoor/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from .models import Company
 
 
 def create_company(request):
     if request.method == "POST":
-        name = request.POST["name"]
-        industry = request.POST["industry"]
-        website = request.POST["website"]
-        logo = request.POST["logo"]
-        structure = request.POST["structure"]
+        try:
+            name = request.POST["name"]
+            industry = request.POST["industry"]
+            website = request.POST["website"]
+            logo = request.FILES["logo"]
+            structure = request.POST["structure"]
+        except Exception as e:
+            return HttpResponse("You did not fill out the form correctly!")
 
         company = Company(name=name, industry=industry, website=website, logo=logo, structure=structure)
         company.save()
@@ -21,17 +24,20 @@ def create_company(request):
 
 
 def view_company(request, pk):
-    company = Company.objects.get(pk=pk)
+    company = get_object_or_404(Company, pk=pk)
     return render(request, "codedoor/viewcompany.html", {"company": company})
 
 
 def edit_company(request, pk):
-    company = Company.objects.get(pk=pk)
+    company = get_object_or_404(Company, pk=pk)
     if request.method == "POST":
-        company.name = request.POST["name"]
-        company.industry = request.POST["industry"]
-        company.website = request.POST["website"]
-        company.structure = request.POST["structure"]
+        try:
+            company.name = request.POST["name"]
+            company.industry = request.POST["industry"]
+            company.website = request.POST["website"]
+            company.structure = request.POST["structure"]
+        except Exception as e:
+            return HttpResponse("You did not fill out the form correctly!")
 
         company.save()
 
