28194c793ebcb25d0e6ba779dd973a3b830d7bbd
Aditya Srivastava <aditya.srivastava@berkeley.edu>
2018-03-18 17:56:55 -0700

    createreview now works with foreign keys for company and profile

--- codedoor/views/reviews.py
+++ codedoor/views/reviews.py
@@ -1,33 +1,49 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from codedoor.models import Company
+from codedoor.models import Review, Company, Profile
+
+#class Review(models.Model):
+#    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+#    reviewer = models.ForeignKey(Profile, on_delete=models.CASCADE)
+#    rating = models.DecimalField(decimal_places=2, max_digits=10)
+#    recommend = models.BooleanField()
+#    review = models.TextField()
+#    title = models.CharField(max_length=200, null=True, blank=True)
+
+#    def __str__(self):
+#        return "{}'s review of {}".format(self.reviewer.name,
+#                                          self.company.name)
 
 def create_review(request):
     if request.method == "POST":
         try:
-            name = request.POST["name"]
-            industry = request.POST["industry"]
-            website = request.POST["website"]
-            logo = request.FILES["logo"]
-            structure = request.POST["structure"]
+            company = Company.objects.get(pk=request.POST["company"])
+            reviewer = Profile.objects.get(pk=request.POST["reviewer"])
+            name=request.POST["reviewer"]
+            rating = request.POST["rating"]
+            recommend = request.POST["recommend"]
+            review = request.POST["review"]
+            title = request.POST["title"]
         except Exception as e:
             return HttpResponse("You did not fill out the form correctly!")
 
-        review = Company(name=name, industry=industry, website=website, logo=logo, structure=structure)
+        review = Review(company=company, reviewer=reviewer, rating=rating, recommend=recommend, review=review, title=title)
         review.save()
 
         return redirect('viewreview/' + str(review.pk))
     else:
-        return render(request, "codedoor/createreview.html")
+        companies = Company.objects.all()
+        reviewers = Profile.objects.all()
+        return render(request, "codedoor/createreview.html", {"companies": companies, "reviewers": reviewers})
 
 
 def view_review(request, pk):
-    review = get_object_or_404(Company, pk=pk)
+    review = get_object_or_404(Review, pk=pk)
     return render(request, "codedoor/viewreview.html", {"review": review})
 
 
 def edit_review(request, pk):
-    review = get_object_or_404(Company, pk=pk)
+    review = get_object_or_404(Review, pk=pk)
     if request.method == "POST":
         try:
             review.name = request.POST["name"]
