19dcee01d40baa8c27c11aa8925cb7c7da9f81e2
Raymond Xu <raymondxu@calvisitor-10-105-140-171.calvisitor.1918.berkeley.edu>
2018-03-20 23:52:15 -0700

    createapplication page

--- codedoor/urls/applications.py
+++ codedoor/urls/applications.py
@@ -4,4 +4,9 @@ from django.urls import path
 
 import codedoor.views as views
 
-urlpatterns = []
+urlpatterns = [
+	path('createquestion', views.create_application, name="create_application"),
+    path('editquestion/<int:pk>', views.edit_application, name="edit_application"),
+    path('viewquestion/<int:pk>', views.view_application, name="view_application"),
+]
+
--- codedoor/views/applications.py
+++ codedoor/views/applications.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from codedoor.models import Profile, Company, Question, Application
+
+def create_application(request):
+	if request.method == 'POST':
+        try:
+            description = request.POST['description']
+            season = request.POST['season']
+            position = request.POST['position']
+            receieved_offer = request.POST['recieved offer']
+            offer_details = request.POST['offer detials']
+            difficulty = request.POST['difficulty']
+        except Exception as e:
+            return HttpResponse("You did not fill out the form correctly")
+
+        a = Application(company=Company.objects.get(pk=1), profile=Profile.objects.get(pk=1), season=season, position=position, received_offer=received_offer, offer_details=offer_details, difficult=difficulty)
+        a.save()
+        return redirect("codedoor:view_application", pk=q.id)
+    else:
+        return render(request, 'codedoor/createapplication.html')
+
+# 
+#def edit_application(request):
+
+
+#company, profiledescription = models.TextField()
+    # year = models.IntegerField()
+    #season = models.CharField(max_length=100, choices=SEASONS)
+    #position = models.CharField(max_length=500)
+    #received_offer = models.BooleanField()
+    #offer_details = models.TextField(null=True, blank=True)
+    #difficult = models.DecimalField(decimal_places=2, max_digits=10)
+
