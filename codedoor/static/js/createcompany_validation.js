59c5409383edcb0ce35e994aac8a1861bf56c5df
Aditya Srivastava <aditya.srivastava@berkeley.edu>
2018-03-30 11:27:11 -0700

    createreview form validation with js works

--- codedoor/static/js/createcompany_validation.js
+++ codedoor/static/js/createcompany_validation.js
@@ -1,20 +1,21 @@
 function validate() {
   var form = document.getElementsByTagName('form')[0];
-  var name = document.getElementById('name').value;
-  var industry = document.getElementById('industry').value;
-  var website = document.getElementById('website').value;
-  var startup = document.getElementById('startup').checked;
-  var boutique = document.getElementById('boutique').checked;
-  var small = document.getElementById('small').checked;
-  var medium = document.getElementById('medium').checked;
-  var large = document.getElementById('large').checked;
-
-  var name_error = document.getElementById('name-error');
-  var industry_error = document.getElementById('industry-error');
-  var website_error = document.getElementById('website-error');
-  var type_error = document.getElementById('type-error');
 
   form.addEventListener('submit', function(event) {
+    var name = document.getElementById('name').value;
+    var industry = document.getElementById('industry').value;
+    var website = document.getElementById('website').value;
+    var startup = document.getElementById('startup').checked;
+    var boutique = document.getElementById('boutique').checked;
+    var small = document.getElementById('small').checked;
+    var medium = document.getElementById('medium').checked;
+    var large = document.getElementById('large').checked;
+
+    var name_error = document.getElementById('name-error');
+    var industry_error = document.getElementById('industry-error');
+    var website_error = document.getElementById('website-error');
+    var type_error = document.getElementById('type-error');
+
     if (!name) {
       name_error.innerHTML = 'You must provide a company name';
       event.preventDefault();
