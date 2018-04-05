691cfdfff28fef9036335e425005b09893eda602
Selina <selina.feng99@gmail.com>
2018-04-05 01:33:50 -0700

    all merge conflicts should be fixed now

--- codedoor/static/js/question.js
+++ codedoor/static/js/question.js
@@ -1,10 +1,6 @@
 function showAAnswer() {
     var x = document.getElementById("applicantAnswer");
-<<<<<<< HEAD
     if (x.style.display !== "block") {
-=======
-    if (x.style.display === "none") {
->>>>>>> c16ebfbafb01656e730eab4554447671c436eec6
         x.style.display = "block";
     } else {
         x.style.display = "none";
@@ -13,11 +9,7 @@ function showAAnswer() {
 
 function showCAnswer() {
     var x = document.getElementById("companyAnswer");
-<<<<<<< HEAD
     if (x.style.display !== "block") {
-=======
-    if (x.style.display === "none") {
->>>>>>> c16ebfbafb01656e730eab4554447671c436eec6
         x.style.display = "block";
     } else {
         x.style.display = "none";
