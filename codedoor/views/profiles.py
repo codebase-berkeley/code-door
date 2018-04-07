80ed2d57fd470823cbdb59585c43b29af89f544a
Aidan Wang <ajwang@berkeley.edu>
2018-04-07 16:29:22 -0700

    Change nav bar profile to automatically get user profile pic

--- codedoor/views/profiles.py
+++ codedoor/views/profiles.py
@@ -14,10 +14,6 @@ from api_keys import s3_access_keys
 profile_pic_bucket = 'codedoor-profile-pics'
 
 
-def get_current_profile(request):
-    return request.user.profile
-
-
 def createprofile(request):
     if request.method == "GET":
         return render(request, 'codedoor/createprofile.html')
@@ -80,8 +76,7 @@ def finishprofile(request):
 @login_required
 def viewprofile(request, pk):
     profile = get_object_or_404(Profile, id=pk)
-    current_profile = get_current_profile(request)
-    return render(request, 'codedoor/viewprofile.html', {"profile": profile, "current_profile": current_profile})
+    return render(request, 'codedoor/viewprofile.html', {"profile": profile})
 
 
 @login_required
@@ -90,9 +85,8 @@ def editprofile(request, pk):
         return redirect("codedoor:viewprofile", pk=pk)  # where to redirect the wrong user trying to edit
     profile = get_object_or_404(Profile, pk=pk)
     user = profile.user
-    current_profile = get_current_profile(request)
     if request.method == "GET":
-        return render(request, 'codedoor/editprofile.html', {"profile": profile, "current_profile": current_profile})
+        return render(request, 'codedoor/editprofile.html', {"profile": profile})
     else:
         try:
             profile.user.email = request.POST['email']
