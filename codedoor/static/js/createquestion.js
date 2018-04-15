347a0c41ff8a354801f2feab6e574e6b27c6b203
Raymond Xu <raymondxu@calvisitor-10-105-143-7.calvisitor.1918.berkeley.edu>
2018-04-14 19:44:03 -0700

    added formdata for a button

--- codedoor/static/js/createquestion.js
+++ codedoor/static/js/createquestion.js
@@ -6,3 +6,24 @@ function displayQuestionForm() {
         x.style.display = "none";
     }
 }
+
+console.log(document.getElementById("submit_button"));
+
+document.getElementById("submit_button").addEventListener("click", function(e) {
+  var question = document.getElementById("question").value;
+  var applicant_ans = document.getElementById("applicant_answer").value;
+  var company_ans = document.getElementById("company_answer").value;
+
+  console.log(question + applicant_ans + company_ans);
+  var formData = new FormData();
+
+  formData.append("question", question);
+  formData.append("applicant_answer", applicant_ans);
+  formData.append("company_answer", company_ans);
+
+  console.log(formData["question"]);
+  fetch("example.com/hello", {
+    method: "POST",
+    body: formData,
+  });
+});
