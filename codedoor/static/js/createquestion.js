d3369641b2646f9690a25228336e439bfdcb4265
Raymond Xu <raymondxu@calvisitor-10-105-143-7.calvisitor.1918.berkeley.edu>
2018-04-14 17:43:52 -0700

    added a javascript file

---
+++ codedoor/static/js/createquestion.js
@@ -0,0 +1,8 @@
+function displayQuestionForm() {
+    var x = document.getElementById("hidden");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
