98c79fbb1a202eb9b7f307693f61e62f89e856f3
sophialau <sophialau@github.com>
2018-04-15 18:06:52 -0700

    catch 22 im tired

--- codedoor/views/root.py
+++ codedoor/views/root.py
@@ -11,57 +11,22 @@ from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 
 @login_required
 def home(request):
-    if request.method == "GET":
-        reviews = Review.objects.all().order_by('-id')
-        applications = Application.objects.all().order_by('-id')
-        paginator_1 = Paginator(reviews, 3)
-        paginator_2 = Paginator(applications, 3)
-        page = request.GET.get('page', 1)
-        try:
-            review_list = paginator_1.page(page)
-        except PageNotAnInteger:
-            review_list = paginator_1.page(1)
-        except EmptyPage:
-            review_list = paginator_1.page(paginator_1.num_pages)
+    reviews = Review.objects.all().order_by('-id')
+    applications = Application.objects.all().order_by('-id')
+    paginator_1 = Paginator(reviews, 3)
+    paginator_2 = Paginator(applications, 3)
+    page = request.GET.get('page', 1)
+    try:
+        review_list = paginator_1.page(page)
+    except PageNotAnInteger:
+        review_list = paginator_1.page(1)
+    except EmptyPage:
+        review_list = paginator_1.page(paginator_1.num_pages)
 
-        try:
-            application_list = paginator_2.page(page)
-        except PageNotAnInteger:
-            application_list = paginator_2.page(1)
-        except EmptyPage:
-            application_list = paginator_2.page(paginator_2.num_pages)
-
-
-        return render(request, "codedoor/home.html", {"reviews": review_list, "applications": application_list})
-    else:
-        try:
-            input_query = request.get("input_query")
-            parsed_query = input_query.split(" ")
-            query = ""
-            for q in parsed_query:
-                query = str(q) + "_"
-            query -= "_"
-            return redirect("codedoor:search", query=query)
-
-
-def search(request, database, text):
-    review_vector = SearchVector('company__name', 'reviewer__user__first_name', 'reviewer__user__last_name', 'title')
-    application_vector = SearchVector('company__name', 'profile__user__first_name', 'profile__user__last_name',
-                                      'position')
-    company_vector = SearchVector('name', 'industry')
-    profile_vector = SearchVector('user__first_name', 'user__last_name', 'current_job')
-
-    if request.method == "GET":
-    if database == "Review":
-        vector = SearchVector('company__name', 'reviewer__user__first_name', 'reviewer__user__last_name', 'title')
-    elif database == "Application":
-        vector = SearchVector('company__name', 'profile__user__first_name', 'profile__user__last_name', 'position')
-    elif database == "Company":
-        vector = SearchVector('name', 'industry')
-    elif database == "Profile":
-        vector = SearchVector('user__first_name', 'user__last_name', 'current_job')
-
-
-Entry.objects.annotate(search).filter(search=text)
-    parsed_text = input
-    query = SearchQuery(text)
+    try:
+        application_list = paginator_2.page(page)
+    except PageNotAnInteger:
+        application_list = paginator_2.page(1)
+    except EmptyPage:
+        application_list = paginator_2.page(paginator_2.num_pages)
+    return render(request, "codedoor/home.html", {"reviews": review_list, "applications": application_list})
--- codedoor/views/search.py
+++ codedoor/views/search.py
@@ -7,37 +7,56 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 
 
-def search(request, database, text):
-    if request.method == "GET":
-    if database == "Review":
-        vector = SearchVector('company__name', 'reviewer__user__first_name', 'reviewer__user__last_name', 'title')
-    elif database == "Application":
-        vector = SearchVector('company__name', 'profile__user__first_name', 'profile__user__last_name', 'position')
-    elif database == "Company":
-        vector = SearchVector('name', 'industry')
-    elif database == "Profile":
-        vector = SearchVector('user__first_name', 'user__last_name', 'current_job')
+def search(request, database):
+    review_vector = SearchVector('company__name', 'reviewer__user__first_name', 'reviewer__user__last_name',
+                                 'title')
+    application_vector = SearchVector('company__name', 'profile__user__first_name', 'profile__user__last_name',
+                                      'position')
+    company_vector = SearchVector('name', 'industry')
+    profile_vector = SearchVector('user__first_name', 'user__last_name', 'current_job')
+    if database == "reviews":
+        vector = review_vector
+    elif database == "applications":
+        vector = application_vector
+    elif database == "companies":
+        vector = company_vector
+    elif database == "profiles":
+        vector = profile_vector
+    input_query = request.POST['query']
+    parsed_query = input_query.split(" ")
+    query = None
+    if parsed_query:
+        query = parsed_query[0]
+    for q in parsed_query[1:]:
+        query = query and SearchQuery(q)
+
+    entry = Entry.objects.annotate(vector).filter(search=query)
+    # review_entry = Entry.objects.annotate(review_vector).filter(search=query)
+    # application_entry = Entry.objects.annotate(application_vector).filter(search=query)
+    # company_entry = Entry.objects.annotate(company_vector).filter(search=query)
+    # profile_entry = Entry.objects.annotate(profile_vector).filter(search=query)
+
+    paginator = Paginator(entry, 10)
+    # review_paginator = Paginator(review_entry, 3)
+    # application_paginator = Paginator(application_entry, 3)
+    # company_paginator = Paginator(company_entry, 3)
+    # profile_paginator = Paginator(profile_entry, 3)
+    page = request.GET.get('page', 1)
+    def pagination(paginator, page):
+        try:
+            return paginator.page(page)
+        except PageNotAnInteger:
+            return paginator.page(1)
+        except EmptyPage:
+            return paginator.page(paginator.num_pages)
+
+    list = pagination(paginator, page)
+    # review_list = pagination(review_paginator, page)
+    # application_list = pagination(application_paginator,page)
+    # company_list = pagination(company_paginator, page)
+    # profile_list = pagination(profile_paginator,page)
+
+    return render(request, "codedoor/search.html", {"database": list})
 
 
-Entry.objects.annotate(search).filter(search=text)
-    parsed_text =
-    query = SearchQuery(text)
 
-if request.method == "GET":
-    return render(request, 'codedoor/createprofile.html')
-else:
-    try:
-        input_username = request.POST['email']
-        input_password = request.POST['password']
-        input_email = request.POST['email']
-        input_first_name = request.POST['first_name']
-        input_last_name = request.POST['last_name']
-        input_profile_pic = request.FILES['profile_pic'].read()
-        input_graduation_year = request.POST['graduation_year']
-        input_current_job = request.POST['current_job']
-        input_linkedin = request.POST['linkedin']
-        if "http://" not in input_linkedin and "https://" not in input_linkedin and input_linkedin:
-            input_linkedin = "http://" + input_linkedin
-        # input_resume = request.POST['resume']
-    except Exception as e:
-        return HttpResponse("You did not fill out the form correctly!")
