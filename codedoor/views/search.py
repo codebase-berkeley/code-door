8ff08c51f2d55015ffd4670e1515a6379fd1b0af
sophialau <sophialau@github.com>
2018-04-15 18:18:26 -0700

    Entry is not a real thing

--- codedoor/views/search.py
+++ codedoor/views/search.py
@@ -8,20 +8,6 @@ from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 
 
 def search(request, database):
-    review_vector = SearchVector('company__name', 'reviewer__user__first_name', 'reviewer__user__last_name',
-                                 'title')
-    application_vector = SearchVector('company__name', 'profile__user__first_name', 'profile__user__last_name',
-                                      'position')
-    company_vector = SearchVector('name', 'industry')
-    profile_vector = SearchVector('user__first_name', 'user__last_name', 'current_job')
-    if database == "reviews":
-        vector = review_vector
-    elif database == "applications":
-        vector = application_vector
-    elif database == "companies":
-        vector = company_vector
-    elif database == "profiles":
-        vector = profile_vector
     input_query = request.POST['query']
     parsed_query = input_query.split(" ")
     query = None
@@ -30,17 +16,25 @@ def search(request, database):
     for q in parsed_query[1:]:
         query = query and SearchQuery(q)
 
-    entry = Entry.objects.annotate(vector).filter(search=query)
-    # review_entry = Entry.objects.annotate(review_vector).filter(search=query)
-    # application_entry = Entry.objects.annotate(application_vector).filter(search=query)
-    # company_entry = Entry.objects.annotate(company_vector).filter(search=query)
-    # profile_entry = Entry.objects.annotate(profile_vector).filter(search=query)
-
+    review_vector = SearchVector('company__name', 'reviewer__user__first_name', 'reviewer__user__last_name',
+                                 'title')
+    application_vector = SearchVector('company__name', 'profile__user__first_name', 'profile__user__last_name',
+                                      'position')
+    company_vector = SearchVector('name', 'industry')
+    profile_vector = SearchVector('user__first_name', 'user__last_name', 'current_job')
+    if database == "Review":
+        vector = review_vector
+        entry = Review.objects.annotate(vector).filter(search=query)
+    elif database == "Application":
+        vector = application_vector
+        entry = Application.objects.annotate(vector).filter(search=query)
+    elif database == "Company":
+        vector = company_vector
+        entry = Company.objects.annotate(vector).filter(search=query)
+    elif database == "Profile":
+        vector = profile_vector
+        entry = Profile.objects.annotate(vector).filter(search=query)
     paginator = Paginator(entry, 10)
-    # review_paginator = Paginator(review_entry, 3)
-    # application_paginator = Paginator(application_entry, 3)
-    # company_paginator = Paginator(company_entry, 3)
-    # profile_paginator = Paginator(profile_entry, 3)
     page = request.GET.get('page', 1)
     def pagination(paginator, page):
         try:
@@ -51,10 +45,6 @@ def search(request, database):
             return paginator.page(paginator.num_pages)
 
     list = pagination(paginator, page)
-    # review_list = pagination(review_paginator, page)
-    # application_list = pagination(application_paginator,page)
-    # company_list = pagination(company_paginator, page)
-    # profile_list = pagination(profile_paginator,page)
 
     return render(request, "codedoor/search.html", {"database": list})
 
