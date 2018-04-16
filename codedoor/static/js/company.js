ceecf472dc04ff68f8b0881c80c42f4ec692051d
Raymond Xu <raymondxu@calvisitor-10-105-135-70.calvisitor.1918.berkeley.edu>
2018-04-15 22:10:13 -0700

    oops forgot this

---
+++ codedoor/static/js/company.js
@@ -0,0 +1,8 @@
+function displayCompanyForm() {
+    var x = document.getElementById("company_form");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
