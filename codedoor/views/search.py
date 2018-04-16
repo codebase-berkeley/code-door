d4fe3ffac2d38ce530eb5da278e6f45799a13fd2
Aidan Wang <ajwang@berkeley.edu>
2018-04-15 18:10:50 -0700

    Change searchresults html

--- codedoor/views/search.py
+++ codedoor/views/search.py
@@ -9,17 +9,17 @@ from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 
 def search(request, database, text):
     if request.method == "GET":
-    if database == "Review":
-        vector = SearchVector('company__name', 'reviewer__user__first_name', 'reviewer__user__last_name', 'title')
-    elif database == "Application":
-        vector = SearchVector('company__name', 'profile__user__first_name', 'profile__user__last_name', 'position')
-    elif database == "Company":
-        vector = SearchVector('name', 'industry')
-    elif database == "Profile":
-        vector = SearchVector('user__first_name', 'user__last_name', 'current_job')
+    # if database == "Review":
+    #     vector = SearchVector('company__name', 'reviewer__user__first_name', 'reviewer__user__last_name', 'title')
+    # elif database == "Application":
+    #     vector = SearchVector('company__name', 'profile__user__first_name', 'profile__user__last_name', 'position')
+    # elif database == "Company":
+    #     vector = SearchVector('name', 'industry')
+    # elif database == "Profile":
+    #     vector = SearchVector('user__first_name', 'user__last_name', 'current_job')
 
-    Entry.objects.annotate(search).filter(search=text)
-        parsed_text =
+    # Entry.objects.annotate(search).filter(search=text)
+        # parsed_text =
         query = SearchQuery(text)
 
     if request.method == "GET":
