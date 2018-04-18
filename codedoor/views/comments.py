857477678c9b3bb94c7e6eaa4777af02cdfb77dd
Steven Tsay <commanderchewbacca@berkeley.edu>
2018-04-17 18:24:50 -0700

    comments workrk now yay, working on cleaning up javascript

--- codedoor/views/comments.py
+++ codedoor/views/comments.py
@@ -1,15 +1,17 @@
 from __future__ import unicode_literals
 from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonResponse
 from django.shortcuts import render, get_object_or_404
-from codedoor.models import ReviewComment
+from codedoor.models import ReviewComment, Review, Profile
 from django.urls import reverse
 
 
 
 
 def addrc(request):
+    print("Called")
     if request.method == "POST":
         review = Review.objects.get(pk=request.POST["review"])
+        print(review)
         title = request.POST['title']
         content = request.POST['content']
         commenter = Profile.objects.get(user=request.user)
