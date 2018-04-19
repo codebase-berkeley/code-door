a11b7ad9aec0442ea820c0275c92198d008cf3ee
Ishan V <ishan.vachhani@gmail.com>
2018-04-18 18:53:11 -0700

    create review ajaxified, just need to fix aesthetics

--- codedoor/views/reviews.py
+++ codedoor/views/reviews.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from codedoor.models import Review, Company, Profile
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
 @login_required
 def create_review(request):
