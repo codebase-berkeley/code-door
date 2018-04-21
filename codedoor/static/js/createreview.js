81bca4b9db6670d6cfd813ca415f89a3624efd66
Ishan V <ishan.vachhani@gmail.com>
2018-04-21 01:05:44 -0700

    fixed validation for create review

--- codedoor/static/js/createreview.js
+++ codedoor/static/js/createreview.js
@@ -7,7 +7,7 @@ function getCookie(name) {
 
 function displayReviewForm() {
     console.log("displaying review form")
-    var x = document.getElementById("hidden");
+    var x = document.getElementById("hidden_review");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
@@ -16,18 +16,20 @@ function displayReviewForm() {
 }
 
 
-document.getElementById("submit_button").addEventListener("click", function(e) {
+document.getElementById("submit_button_review").addEventListener("click", function(e) {
+  var error_exists = validate_review();
   var reviewtitle = document.getElementById("title1").value;
   var pk = document.getElementById("reviewcompany").value;
   var rating = document.getElementById("rating").value;
   var recommend = document.getElementsByName("recommend");
+  var review = document.getElementById("review").value;
+
   if(recommend[0].checked) {
     recommend = "True";
   }
   else if(recommend[1].checked) {
     recommend = "False";
   }
-  var review = document.getElementById("review").value;
   // document.getElementById("question").value = "";
   // document.getElementById("applicant_answer").value = "";
   // document.getElementById("company_answer").value = "";
@@ -44,13 +46,21 @@ document.getElementById("submit_button").addEventListener("click", function(e) {
   var headers = new Headers();
   var csrftoken = getCookie("csrftoken")  
   headers.append('X-CSRFToken', csrftoken);
+
+  console.log(error_exists);
+  if(!error_exists) {
+  document.getElementById("title1").value = "";
+  document.getElementById("rating").value = "";
+  document.getElementsByName("recommend")[0].checked = false;
+  document.getElementsByName("recommend")[1].checked = false;
+  document.getElementById("review").value = "";
+    displayReviewForm();
   fetch("/codedoor/createdreview", {
     method: "POST",
     body: formData,
     headers: headers,
     credentials: "include"
   }).then(function(response) {
-    console.log("made it here")
     return response.json();
   }).then(function(json) {
     if (json.success) {
@@ -66,5 +76,56 @@ document.getElementById("submit_button").addEventListener("click", function(e) {
         console.log("we made it");
     }
   })
-  });
+}
+});
 
+function validate_review() {
+  var error_exists = false;
+
+    var title = document.getElementById('title1').value;
+    var rating = document.getElementById('rating').value;
+    var yes = document.getElementById('yes').checked;
+    var no = document.getElementById('no').checked;
+    var review = document.getElementById('review').value;
+
+    var title_error = document.getElementById('title-error');
+    var rating_error = document.getElementById('rating-error');
+    var review_error = document.getElementById('review-error');
+    var recommend_error = document.getElementById('recommend-error');
+    var display_error = document.getElementById('display-error');
+    display_error.innerHTML = '';
+
+    if (!title || title.trim().length == 0 || title === 'None') {
+      error_exists = true;
+      display_error.innerHTML += 'You must provide a title <br>';
+      title_error.innerHTML = 'You must provide a title';
+      event.preventDefault();
+    } else {
+      title_error.innerHTML = '';
+    }
+    if (!rating || rating > 5 || rating < 0) {
+      error_exists = true;
+      display_error.innerHTML += 'You must provide a valid rating<br>';
+      rating_error.innerHTML = 'You must provide a valid rating';
+      event.preventDefault();
+    } else {
+      rating_error.innerHTML = '';
+    }
+    if (!review || review.trim().length == 0 || review === 'None') {
+      error_exists = true;
+      display_error.innerHTML += 'You must provide a review <br>';
+      review_error.innerHTML = 'You must provide a review';
+      event.preventDefault();
+    } else {
+      review_error.innerHTML = '';
+    }
+    if (!yes && !no) {
+      error_exists = true;
+      display_error.innerHTML += 'You must provide a recommendation <br>';
+      recommend_error.innerHTML = 'You must provide a recommendation';
+      event.preventDefault();
+    } else {
+      recommend_error.innerHTML = '';
+    }
+    return error_exists;
+}
