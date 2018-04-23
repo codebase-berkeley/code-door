54e4467ea3e0f6ae4c62a1c98dd49755b9b0e986
ekwkk <ericakong0331@gmail.com>
2018-04-22 22:02:48 -0700

    csv-companies, still needs to upload to s3

--- csv-companies.py
+++ csv-companies.py
@@ -1,35 +1,62 @@
 import csv
-from codedoor.models import Company
+import json
+import requests
+
+lst = []
 
 with open('companies.csv', newline='', encoding='utf-8') as f:
+    next(f)
     reader = csv.reader(f)
+    pk = 1
     for row in reader:
-        print('Company name:' + row[1])
-        name = row[1]
-        print('Industry: ' + row[3])
-        industry = row[3]
-        print('Website:' + row[2])
-        website = row[2]
-        fund = 0;
+        websites = row[2].split(';')
+        website = websites[0]
+        if website[0:5] == 'https':
+            url = website[8:-1]
+        else:
+            url = website[7:-1]
+
+        img = requests.get('https://logo.clearbit.com/' + url)
+        if img.status_code == 200:
+            img_data = img.content
+
+            # NEEDS TO UPLOAD TO S3
+            # with open('static/images/logos/' + name + '.png', 'wb') as handler:
+            #     handler.write(img_data)
+        else:
+            continue
+
+        name = row[1][:100]
+        industry = row[3][:100]
+        fund = 0
         structure = 'Startup'
         try:
-        	fund = int(row[4])
+            fund = int(row[4])
         except ValueError:
-        	pass
+            pass
         if fund > 2000000:
-        	print('Structure: Large')
-        	structure = 'Large'
+            structure = 'Large'
         elif fund > 1000000:
-        	print('Structure: Medium')
-        	structure = 'Medium'
+            structure = 'Medium'
         elif fund > 500000:
-        	print('Structure: Small')
-        	structure = 'Small'
+            structure = 'Small'
         elif fund > 250000:
-        	print('Structure: Boutique')
-        	structure = 'Boutique'
+            structure = 'Boutique'
         else:
-        	print('Structure: Startup')
-        	structure = 'Startup'
-        company = Company(name=name, industry=industry, website=website, structure=structure)
-        company.save()
+            structure = 'Startup'
+
+        company_fields = {"name": name,
+                          "industry": industry,
+                          "website": website,
+                          "logo": None,
+                          "structure": structure}
+        company = {"model": "codedoor.company",
+                   "pk": pk,
+                   "fields": company_fields}
+        lst.append(company)
+        pk += 1
+
+with open('codedoor/fixtures/companies.json', 'w+') as g:
+    g.write(json.dumps(lst))
+
+
