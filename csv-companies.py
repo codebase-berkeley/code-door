acb4ff8f3d12526fff95b860967c1af6c33e5111
Aditya Srivastava <aditya.srivastava@berkeley.edu>
2018-04-23 23:30:16 -0700

    company logo to s3 url

--- csv-companies.py
+++ csv-companies.py
@@ -1,6 +1,10 @@
 import csv
 import json
 import requests
+import boto3
+from api_keys import s3_access_keys
+
+company_logos_bucket = 'codedoor-company-logos'
 
 lst = []
 
@@ -19,6 +23,14 @@ with open('companies.csv', newline='', encoding='utf-8') as f:
         img = requests.get('https://logo.clearbit.com/' + url)
         if img.status_code == 200:
             img_data = img.content
+
+            s3 = boto3.resource('s3', aws_access_key_id=s3_access_keys["id"],
+                                aws_secret_access_key=s3_access_keys["secret"])
+            s3.Bucket(company_logos_bucket).put_object(Key=str(pk), Body=img_data, ACL='public-read')
+
+            url = "https://s3-us-west-1.amazonaws.com/" + company_logos_bucket + "/" + str(pk)
+            print(url)
+            logo = url
 
             # NEEDS TO UPLOAD TO S3
             # with open('static/images/logos/' + name + '.png', 'wb') as handler:
@@ -48,7 +60,7 @@ with open('companies.csv', newline='', encoding='utf-8') as f:
         company_fields = {"name": name,
                           "industry": industry,
                           "website": website,
-                          "logo": None,
+                          "logo": logo,
                           "structure": structure}
         company = {"model": "codedoor.company",
                    "pk": pk,
@@ -58,5 +70,3 @@ with open('companies.csv', newline='', encoding='utf-8') as f:
 
 with open('codedoor/fixtures/companies.json', 'w+') as g:
     g.write(json.dumps(lst))
-
-
