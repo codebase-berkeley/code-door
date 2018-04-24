2005317205a8c06ae2a813c2c5cfd415b1e44c32
Raymond Xu <raymondxu@calvisitor-10-105-141-235.calvisitor.1918.berkeley.edu>
2018-04-23 18:08:17 -0700

    modal for create question:

--- codedoor/static/js/createquestion.js
+++ codedoor/static/js/createquestion.js
@@ -10,7 +10,7 @@ function getCookie(name) {
 
 
 function displayQuestionForm() {
-    var x = document.getElementById("hidden");
+    var x = document.getElementById("myModal");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
@@ -27,6 +27,40 @@ function displayEditApplicationForm() {
         x.style.display = "none";
     }
 }
+
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("qBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
+
+
 
 
 
@@ -72,6 +106,12 @@ document.getElementById("submit_button").addEventListener("click", function(e) {
     }
   })
   });
+
+
+
+
+
+
 
 
 
