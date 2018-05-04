ab6d36fce06aac4c64a999c8be6dc3f004034084
bdeleonardis1 <bdeleonardis1@gmail.com>
2018-05-03 22:21:47 -0700

    The last try

--- codedoor/views/profiles.py
+++ codedoor/views/profiles.py
@@ -176,8 +176,7 @@ def slack_callback(request):
         r = requests.post(get_token_url,
                           auth=HTTPBasicAuth(client_id, client_secret),
                           headers={"content-type": "application/x-www-form-urlencoded"},
-                          params={"code": code, "grant_type": "authorization_code",
-                                  "redirect_uri": "{}/codedoor/slack_info".format(absolute_url)})
+                          params={"code": code, "grant_type": "authorization_code"})
         access_token = r.json()['access_token']
 
         get_activity_url = "https://slack.com/api/users.identity"
