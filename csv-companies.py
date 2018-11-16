f2fa2b987fd24edb7c74f819dddc3d7843fb2184
Andrew Chan <andrewkchan.akc@gmail.com>
2018-11-15 16:56:13 -0800

    Finish processing companies from dump

--- csv-companies.py
+++ csv-companies.py
@@ -6,17 +6,25 @@ import time
 from api_keys import s3_access_keys
 
 company_logos_bucket = 'codedoor-companies-logos'
+s3 = boto3.resource('s3', aws_access_key_id=s3_access_keys["id"],
+                    aws_secret_access_key=s3_access_keys["secret"])
 
 lst = []
+pk_end = 13948
 
 def log(stuff):
     print("TSTAMP: {} LOG: {}".format(time.time(), stuff))
 
-with open('companies.csv', newline='', encoding='utf-8') as f:
+with open('companies.csv', newline='', encoding='utf-8') as f, \
+    open('companies_processed.csv', 'w+', encoding='utf-8') as d:
+
     next(f)
     reader = csv.reader(f)
+    writer = csv.writer(d)
     pk = 1
     for row in reader:
+        if pk >= pk_end:
+            break
         websites = row[2].split(';')
         website = websites[0]
         if website.startswith("http://"):
@@ -33,18 +41,13 @@ with open('companies.csv', newline='', encoding='utf-8') as f:
             if img.status_code == 200:
                 keep_trying = False
                 img_data = img.content
-
-                s3 = boto3.resource('s3', aws_access_key_id=s3_access_keys["id"],
-                                    aws_secret_access_key=s3_access_keys["secret"])
-                s3.Bucket(company_logos_bucket).put_object(Key=str(pk), Body=img_data, ACL='public-read')
-
-                url = "https://s3-us-west-1.amazonaws.com/" + company_logos_bucket + "/" + str(pk)
-                log("Got logo for pk {} at: {}".format(pk, url))
-                logo = url
-
-                # NEEDS TO UPLOAD TO S3
-                # with open('static/images/logos/' + name + '.png', 'wb') as handler:
-                #     handler.write(img_data)
+                try:
+                    s3.Bucket(company_logos_bucket).put_object(Key=str(pk), Body=img_data, ACL='public-read')
+                    url = "https://s3-us-west-1.amazonaws.com/" + company_logos_bucket + "/" + str(pk)
+                    log("Got logo for pk {} at: {}".format(pk, url))
+                    logo = url
+                except Exception as e:
+                    log("Exception thrown when uploading image for pk {}, content {}".format(pk, e))
             elif img.status_code in [400, 404, 504]:
                 log("Error code {}, content {} for pk {}".format(img.content, img.status_code, pk))
                 keep_trying = False
@@ -71,16 +74,17 @@ with open('companies.csv', newline='', encoding='utf-8') as f:
         else:
             structure = 'Startup'
 
-        company_fields = {"name": name,
-                          "industry": industry,
-                          "website": website,
-                          "logo": logo,
-                          "structure": structure}
-        company = {"model": "codedoor.company",
-                   "pk": pk,
-                   "fields": company_fields}
-        lst.append(company)
+        writer.writerow([name, industry, website, logo, structure])
+        # company_fields = {"name": name,
+        #                   "industry": industry,
+        #                   "website": website,
+        #                   "logo": logo,
+        #                   "structure": structure}
+        # company = {"model": "codedoor.company",
+        #            "pk": pk,
+        #            "fields": company_fields}
+        # lst.append(company)
         pk += 1
 
-with open('codedoor/fixtures/companies.json', 'w+') as g:
-    g.write(json.dumps(lst))
+# with open('codedoor/fixtures/companies.json', 'w+') as g:
+#     g.write(json.dumps(lst))
