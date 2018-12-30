ce203acca4a8f91a042a69292f746fed3614a2cb
Andrew Chan <andrewkchan.akc@gmail.com>
2018-12-29 19:40:23 -0800

    Standardize homepage, inputs styling

--- codedoor/static/js/editcompany.js
+++ codedoor/static/js/editcompany.js
@@ -1,14 +1,3 @@
-// window.onload = checked();
-// function checked() {
-// 	var check = "{{company.structure}}";
-// 	console.log(check);
-// 	for (var i = 0; i < document.form.structure.length; i++) {
-// 		if (document.form.structure[i] == check) {
-// 			document.form.structure[i].setAttribute("checked", "true");
-// 		}
-// 	}
-// }
-
 document.addEventListener("DOMContentLoaded", function() {
   var check = {{company.structure}};
   console.log(check);
